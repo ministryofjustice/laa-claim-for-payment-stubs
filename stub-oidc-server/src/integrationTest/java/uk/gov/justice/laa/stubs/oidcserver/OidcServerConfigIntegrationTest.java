0640aad6cc458cea8115b2207af691bf7fa5c2a4
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-03-20 13:17:26 +0000

    CLAS-168: Updated assertion

--- stub-oidc-server/src/integrationTest/java/uk/gov/justice/laa/stubs/oidcserver/OidcServerConfigIntegrationTest.java
+++ stub-oidc-server/src/integrationTest/java/uk/gov/justice/laa/stubs/oidcserver/OidcServerConfigIntegrationTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = MockOidcApplication.class)
@@ -27,6 +27,6 @@ public class OidcServerConfigIntegrationTest {
         mockMvc
             .perform(get("/.well-known/openid-configuration"))
             .andExpect(status().isOk())
-            .andDo(MockMvcResultHandlers.print());
+            .andExpect(jsonPath("$.issuer").value("http://localhost:8081"));
     }
 }
